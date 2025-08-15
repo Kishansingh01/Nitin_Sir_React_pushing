c3dd0e7460791d28e044eb15de1ebab9c75baba5
Kishan <kishansingh9006866@gmail.com>
2025-08-16 01:31:37 +0530

    Inatilize 2

--- index.js
+++ index.js
@@ -10,10 +10,10 @@
 //                         React.createElement("h1",{id:"childInside4"},"Hiiiii i am the Billionaire")
                     
 //                       ]) ]);
-const h1 = React.createElement("h1",{id:"childInside1"},"Hello friends i am before kishan");
-const h2 = React.createElement("h1",{id:"childInside2"},"Hello i am the before theeeeeeeeeee Billionaire");
-const h3 = React.createElement("h1",{id:"childInside3"},"Hiiiii friends i am beforekishan");
-const h4 = React.createElement("h1",{id:"childInside4"},"Hiiiii i am before it");
+const h1 = React.createElement("h1",{id:"childInside1"},"Hello friends i am before  after afterkishan");
+const h2 = React.createElement("h1",{id:"childInside2"},"Hello i am the before after theeeeeeeeeee Billionaire");
+const h3 = React.createElement("h1",{id:"childInside3"},"Hiiiii friends i am before after kishan");
+const h4 = React.createElement("h1",{id:"childInside4"},"Hiiiii i am before  after it");
 const child1=React.createElement("div",{id:"child1"},h1,h2);
 const child2=React.createElement("div",{id:"child2"},h3,h4);
 const heading=React.createElement("div",{id:"parent"},child1,child2);
